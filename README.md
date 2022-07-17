8f411f9b9acf6b785f8293df8041fa55d44b10eb
Zlatko <zlatko.roncevic@gmail.com>
2022-07-17 19:28:56 +0200

    working version

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Calculating minimal path in weighted triangle
 ```
 sbt 'set test in assembly := {}' clean assembly
 ```
+
+
+### Example of starting from root folder
+
+```
+7
+6 3
+3 8 5
+11 2 10 9
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,33 @@
 package com.zlatko
 
+import WeightedTriangle.Node
+
 import org.slf4j.LoggerFactory
+
+import scala.collection.mutable.Stack
+
 
 object Main extends App {
 
   def logger = LoggerFactory.getLogger(this.getClass)
 
-  logger.info("---------- Min triangle path ------------ ")
+  //logger.info("---------- Min triangle path ------------ ")
 
-  for (ln <- io.Source.stdin.getLines) println(ln)
+  var s = Stack[List[Node]]()
+  for (ln <- io.Source.stdin.getLines) {
+
+    val n = ln.split(" ").map(_.trim).toList.map(s => Node(s.toInt))
+    s.push(n)
+    //println(ln)
+  }
+
+  //logger.info(s"Stack: ${s}")
+
+  val path = WeightedTriangle.findMinPath(s)
+  logger.info(s" Minimal path is ${path.nodes.map(n => n.weight).mkString(" + ")} = ${path.nodes.map(n => n.weight).foldLeft(0)(_ + _)}")
+
+
+
 
 
 }
